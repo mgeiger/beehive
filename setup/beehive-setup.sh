c48c0b74294558d0bb6bb58c7a883d58dbc364f7
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2016-12-02 21:49:01 -0500

    Combine all items into one.

---
+++ setup/beehive-setup.sh
@@ -0,0 +1,7 @@
+# Setup the Packages properly
+# Copy over the start scripts
+# Move the files to the /opt/beehive directory
+# Should we do a virtual environment...maybe?
+# Run some tests
+# Start the service
+# Run some more tests
