06ae951c6a55e6d1391ad7721d766bdc9e7766a8
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2016-12-08 19:26:52 -0500

    Updated tests on ADC and DHT

--- beehive/test/test_adc.py
+++ beehive/test/test_adc.py
@@ -3,4 +3,5 @@ import Adafruit_ADS1x15
 adc = Adafruit_ADS1x15.ADS1115()
 
 for i in range(0, 4):
-    print("{}: {}".format(i, adc.read_adc(i)))
+    adc_value = adc.read_adc(i)
+    print("{}: {}".format(i, adc_value) 
--- beehive/test/test_dht.py
+++ beehive/test/test_dht.py
@@ -1,7 +1,10 @@
 import Adafruit_DHT
+import time
 
 DHT_TYPE = 22
 DHT_GPIO = 23
 
-values = Adafruit_DHT.read_retry(DHT_TYPE, DHT_GPIO)
-print('Values: {}'.format(values))
+for i in range(0, 10):
+    values = Adafruit_DHT.read_retry(DHT_TYPE, DHT_GPIO)
+    print('Values: {}'.format(values))
+    time.sleep(2)
