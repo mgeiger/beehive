71860f197e31220a0b8bed12f0577b049b0dd4ce
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2017-12-21 11:03:17 -0500

    Minor changes to update. Nothing big.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Overview
+
+
+# Todo
+- [ ] Layout the Hardware system 
+- [ ] Figure out if I'm going to use the AD Convertor (ADS1x15)
+- [ ] Investigae Weight Sensor system
+- [ ] Need to figure out how to handle the API to get random numbers
+
+# Hardware
+## Sensor Hardware
+1. BMP180
+1. DHT22
+1. DS18B20 (4 or 5x)
+1. Light Sensor ()
+
+## Sensors Locations
+- Temperatures
+  - Bottom Brood Super
+  - Top Brood Super
+  - Bottom Honey Super
+  - Top Honey Super
+  - External
+  - Hive Entrance
+- Humidity
+  - Hive Entrance
+  - Middle of Hive
+  - External
+- Other
+  - Altitude
+  - Light
+
+# Software
+## Database Storage
+Will be using Postgres as our database storage. 
+The other idea is to use InfluxDB or Graphite. 
+Both of these databases are good for time-series data storage.
+
+
+
+# Reference
+## Blogs to View
+
+##### Video Monitoring
+http://dansbeehives.blogspot.com/2013/07/fully-automated-beehive-surveillance.html
+
+##### Bee Monitoring
+http://annemariemaes.net/tag/bee-monitoring/
+
+##### Integrating to Xively Feed
+https://hackaday.io/project/2453-arduino-beehive-monitor
+
+
--- beehive/app.py
+++ beehive/app.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, jsonify, request
-from random import sample
 import json
-import time
-import collections
-import database
+
+from flask import Flask, render_template, jsonify, request
+
+from .database import database
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('chart.html')
-            
+
+
 @app.route('/data')
 def data():
     return jsonify(database.get_averages())
-            
+
+
 @app.route('/add', methods=['POST'])
 def add_data():
     post_data = json.loads(request.data.decode("utf-8"))
     parse_data(post_data)
     return jsonify({"Thank you!": 0})
 
+
 def parse_data(data):
     if isinstance(data, dict):
         try:
             app.logger.debug(data)
-            database.insert_database(temperature=float(data['temperature']), temperature2=float(data['temperature2']), humidity=float(data['humidity']), pressure=float(data['pressure']), altitude=float(data['altitude']), light=float(data['light']))
+            database.insert_database(temperature=float(data['temperature']),
+                                     temperature2=float(data['temperature2']),
+                                     humidity=float(data['humidity']),
+                                     pressure=float(data['pressure']),
+                                     altitude=float(data['altitude']),
+                                     light=float(data['light']))
         except KeyError:
             app.logger.error('Failed to find key in JSON')
     else:
